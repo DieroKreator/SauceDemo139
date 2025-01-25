241baa84773d20136555e581346f6ebe5feaee2e
DieroKreator <qtechwired@gmail.com>
2025-01-25 19:45:46 -0300

    Enhance CommonPage with common element mappings and adjust namespace

--- Pages/CommonPage.cs
+++ Pages/CommonPage.cs
@@ -1,10 +1,15 @@
 using OpenQA.Selenium;
 
-namespace SauceDemo139.Pages
+namespace Pages
 {
     public class CommonPage
     {
+        // Atributos
         protected IWebDriver driver;
+
+        // Mapeamento dos Elementos comuns a duas ou mais páginas
+        private IWebElement lblTituloSecao => driver.FindElement(By.CSSSelector("span.title"));
+        private IWebElement iconCarrinho => driver.FindElement(By.CssSelector("a.shopping_cart_link"));
 
         public CommonPage(IWebDriver driver)
         {

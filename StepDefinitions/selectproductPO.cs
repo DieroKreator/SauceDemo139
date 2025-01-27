3cb5db53c0d0de91b69bdf8fd39953feb08c0cb1
DieroKreator <qtechwired@gmail.com>
2025-01-27 11:04:01 -0300

    Remove not usable lines of code

--- StepDefinitions/selectproductPO.cs
+++ StepDefinitions/selectproductPO.cs
@@ -1,3 +1,5 @@
+using TechTalk.SpecFlow;
+
 namespace MyNamespace
 {
     [Binding]
@@ -5,7 +7,7 @@ namespace MyNamespace
     {
         public readonly ScenarioContext _scenarioContext;
         private IWebDriver driver;
-        
+
         public SelectProductPO(IWebDriver driver)
         {
             this.driver = driver;
@@ -14,53 +16,46 @@ namespace MyNamespace
         [Given(@"que acesso a página inicial da loja PO")]
         public void DadoQueAcessoAPaginaInicialDaLojaPO()
         {
-            _scenarioContext.Pending();
         }
 
         [When(@"preencho o ""(.*)"" PO")]
         public void QuandoPreenchoOPO(string p0)
         {
-            _scenarioContext.Pending();
         }
 
         [When(@"a ""(.*)"" e clico no botao login PO")]
         public void QuandoAEClicoNoBotaoLoginPO(string p0)
         {
-            _scenarioContext.Pending();
         }
 
         [Then(@"exibe ""(.*)"" no titulo da Selecao PO")]
         public void EntaoExibeNoTituloDaSelecaoPO(string products0)
         {
-            _scenarioContext.Pending();
         }
+
         [When(@"adiciono o ""(.*)"" ao carrinho PO")]
         public void QuandoAdicionoOAoCarrinhoPO(string p0)
         {
-            _scenarioContext.Pending();
         }
+
         [When(@"clico no icone do carrinho de compras PO")]
         public void QuandoClicoNoIconeDoCarrinhoDeComprasPO()
         {
-            _scenarioContext.Pending();
         }
 
         [Then(@"exibe a página do carrinho com a ""(.*)"" PO")]
         public void EntaoExibeAPaginaDoCarrinhoComAPO(string p0)
         {
-            _scenarioContext.Pending();
         }
 
         [Then(@"nome do ""(.*)"" PO")]
         public void EntaoNomeDoPO(string p0)
         {
-            _scenarioContext.Pending();
         }
 
         [Then(@"o preco como ""(.*)"" PO")]
         public void EntaoOPrecoComoPO(string p0)
         {
-            _scenarioContext.Pending();
         }
     }
 }

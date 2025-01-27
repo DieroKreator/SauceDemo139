a4edcc69f97402580eb1b7606ef5a49e5d6d6608
DieroKreator <qtechwired@gmail.com>
2025-01-26 22:27:30 -0300

    Add CartPage class structure

---
+++ Pages/CartPage.cs
@@ -0,0 +1,24 @@
+namespace Pages
+{
+    public class CartPage : CommonPage
+    {
+        private IWebElement lblQuantidade => driver.FindElement(By.CssSelector("div.cart-quantity"));
+        private IWebElement lblNomeProduto => driver.FindElement(By.CssSelector("div.inventory_item_name"));
+        private IWebElement lblPrecoProduto => driver.FindElement(By.CssSelector("div.inventory_item_price"));
+
+        public CartPage(IWebDriver driver) : base(driver){}
+
+        public String LerQuantidade()
+        {
+            return lblQuantidade.Text;
+        }
+        public String LerTituloProduto()
+        {
+            return lblNomeProduto.Text;
+        }   
+        public String LerPreco()
+        {
+            return lblPreco.Text;
+        }
+    }
+}

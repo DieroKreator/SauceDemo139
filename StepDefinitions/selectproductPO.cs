7fc0b4eeeb898403331506fe630ff30ce122d111
DieroKreator <qtechwired@gmail.com>
2025-01-27 20:55:20 -0300

    Add step definitions for SelectProductPO

--- StepDefinitions/selectproductPO.cs
+++ StepDefinitions/selectproductPO.cs
@@ -1,4 +1,5 @@
 using OpenQA.Selenium;
+using Pages;
 using TechTalk.SpecFlow;
 
 namespace MyNamespace
@@ -6,57 +7,80 @@ namespace MyNamespace
     [Binding]
     public class SelectProductPO
     {
-        public readonly ScenarioContext _scenarioContext;
+        private ScenarioContext _scenarioContext;
         private IWebDriver driver;
 
-        public SelectProductPO(IWebDriver driver)
+        private LoginPage loginPage;
+        private CommonPage commonPage;
+        private InventoryPage inventoryPage;
+        private CartPage cartPage;
+
+        public SelectProductPO(ScenarioContext scenarioContext)
         {
-            this.driver = driver;
+            _scenarioContext = scenarioContext;
+            driver = (IWebDriver)_scenarioContext["driver"];
         }
 
         [Given(@"que acesso a página inicial da loja PO")]
         public void DadoQueAcessoAPaginaInicialDaLojaPO()
         {
+            driver.Navigate().GoToUrl("https://www.saucedemo.com/");
         }
 
         [When(@"preencho o ""(.*)"" PO")]
-        public void QuandoPreenchoOPO(string p0)
+        public void QuandoPreenchoOPO(string usuario)
         {
+            loginPage = new LoginPage(driver);
+            loginPage.PreencherUsuario(usuario);
         }
 
         [When(@"a ""(.*)"" e clico no botao login PO")]
-        public void QuandoAEClicoNoBotaoLoginPO(string p0)
+        public void QuandoAEClicoNoBotaoLoginPO(string senha)
         {
+            loginPage.PreencherSenha(senha);
+            loginPage.ClicarNoBotaoLogin();
         }
 
         [Then(@"exibe ""(.*)"" no titulo da Selecao PO")]
-        public void EntaoExibeNoTituloDaSelecaoPO(string products0)
+        public void EntaoExibeNoTituloDaSelecaoPO(string tituloSecao)
         {
+            inventoryPage = new InventoryPage(driver);
+            Assert.That(inventoryPage.LerTituloDaSecao, 
+                        Is.EqualTo(tituloSecao));
         }
 
         [When(@"adiciono o ""(.*)"" ao carrinho PO")]
-        public void QuandoAdicionoOAoCarrinhoPO(string p0)
+        public void QuandoAdicionoOAoCarrinhoPO(string produto)
         {
+            inventoryPage.AdicionarAoCarrinho(produto);
         }
 
         [When(@"clico no icone do carrinho de compras PO")]
         public void QuandoClicoNoIconeDoCarrinhoDeComprasPO()
         {
+            inventoryPage.IrParaCarrinho();
         }
 
         [Then(@"exibe a página do carrinho com a ""(.*)"" PO")]
-        public void EntaoExibeAPaginaDoCarrinhoComAPO(string p0)
+        public void EntaoExibeAPaginaDoCarrinhoComAPO(string quantidade)
         {
+            cartPage = new CartPage(driver);
+            Assert.That(cartPage.LerQuantidade(),
+                        Is.EqualTo(quantidade));
         }
 
         [Then(@"nome do ""(.*)"" PO")]
-        public void EntaoNomeDoPO(string p0)
+        public void EntaoNomeDoPO(string produto)
         {
+            Assert.That(cartPage.LerTituloProduto(),
+                        Is.EqualTo(produto));
         }
 
         [Then(@"o preco como ""(.*)"" PO")]
-        public void EntaoOPrecoComoPO(string p0)
+        public void EntaoOPrecoComoPO(string preco)
         {
+            Assert.That(cartPage.LerPreco(),
+                        Is.EqualTo(preco));
         }
     }
 }

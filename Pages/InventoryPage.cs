120c9337a567d6b1d207f93ce33518a1d4742251
DieroKreator <qtechwired@gmail.com>
2025-01-26 22:14:11 -0300

    Use dynamic ID mapping for product buttons

--- Pages/InventoryPage.cs
+++ Pages/InventoryPage.cs
@@ -4,15 +4,17 @@ namespace Pages
     public class InventoryPage : CommonPage
     {
         // Mapeamento de elementos
+        // Como queremos mapear o botão "Add to Cart" e o id muda por produto
+        // Iremos mapear de forma dinâmica pelo bloco de Ações
 
         // Constructor
          public InventoryPage(IWebDriver driver) : base(driver){}
 
         // Métodos ou ações
-        public void AddToCart(string itemName)
+        public void AdicionarAoCarrinho(string produto)
         {
-            var item = _driver.FindElement(By.XPath($"//div[text()='{itemName}']/following-sibling::button"));
-            item.Click();
+            String seletorProduto = "add-to-cart-" + produto.ToLower().Replace(" ", "_");
+            driver.FindElement(By.Id(seletorProduto)).Click();
         }
 
         public void RemoveFromCart(string itemName)
